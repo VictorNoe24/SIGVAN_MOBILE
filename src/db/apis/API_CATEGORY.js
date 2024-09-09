b4c0cef6447760f1a0f257b1485ff5cc67f231be
VictorNoe24 <noeflo60@gmail.com>
2024-09-09 09:47:47 -0600

    [ADD] search-screen

--- src/db/apis/API_CATEGORY.js
+++ src/db/apis/API_CATEGORY.js
@@ -1,12 +1,8 @@
 import * as SQLite from "expo-sqlite";
-import {databaseName, LOGGER} from "../../utils/env";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import {databaseName, LOGGER, userId} from "../../utils/env";
 
 export const getAllCategory = async () => {
     try {
-        const jsonValue = await AsyncStorage.getItem('AUTH_USER_TOKEN');
-        const data = JSON.parse(jsonValue);
-
         const db = await SQLite.openDatabaseAsync(databaseName)
         return await db.getAllAsync(
             `SELECT * 
@@ -14,9 +10,10 @@ export const getAllCategory = async () => {
                     WHERE id_user=? 
                     ORDER BY name_category DESC 
                     LIMIT 10;`,
-            [data?.id_user]
+            [userId]
         );
     } catch (e) {
         LOGGER.error(e);
     }
 };
+
--- src/utils/env.js
+++ src/utils/env.js
@@ -1,4 +1,12 @@
 import { logger } from "react-native-logs";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const getIdUser = async () => {
+    const jsonValue = await AsyncStorage.getItem('AUTH_USER_TOKEN');
+    const data = JSON.parse(jsonValue);
+    return data?.id_user;
+}
 
 export const LOGGER = logger.createLogger();
 export const databaseName = 'test_database_002.db'
+export const userId = getIdUser();
