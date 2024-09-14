510d7a959a2be1f4f4ce4f1c739f3e347812750f
VictorNoe24 <noeflo60@gmail.com>
2024-09-14 16:47:44 -0600

    [ADD] screen products

--- src/db/apis/API_PRODUCTS.js
+++ src/db/apis/API_PRODUCTS.js
@@ -113,3 +113,44 @@ export const insertPhotos = async (idProduct, images) => {
     }
 }
 
+export const getByIdProduct = async (idProduct) => {
+    try {
+        const db = await SQLite.openDatabaseAsync(databaseName);
+        let data = [];
+
+        const response = await db.getAllAsync(
+            `SELECT p.id_product, p.name_product, p.description, p.purchase_price, p.sale_price, p.stock, p.bar_code, 
+                 p.discount, p.id_status, c.name_category, 
+                 GROUP_CONCAT(pp.url_photo) AS images
+                 FROM products AS p
+                 INNER JOIN categories AS c ON p.id_category = c.id_category
+                 LEFT JOIN photo_products AS pp ON p.id_product = pp.id_product AND pp.id_user = ?
+                 WHERE p.id_user = ? AND p.id_product = ?
+                 GROUP BY p.id_product
+                 ORDER BY p.created_at DESC;`,
+            [await userId, await userId, idProduct]
+        );
+
+        for (const product of response) {
+            const photos = product?.images ? product.images.split(',') : [];
+            data.push({
+                id_product: product?.id_product,
+                name_product: product?.name_product,
+                description: product?.description,
+                purchase_price: product?.purchase_price,
+                sale_price: product?.sale_price,
+                stock: product?.stock,
+                bar_code: product?.bar_code,
+                discount: product?.discount,
+                id_status: product?.id_status,
+                name_category: product?.name_category,
+                images: photos
+            });
+        }
+        return data;
+
+    } catch (e) {
+        LOGGER.error(e);
+    }
+}
+
