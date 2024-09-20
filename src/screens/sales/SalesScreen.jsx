016143fc7c35d79d387ec7b42d14767a1145d75f
VictorNoe <20213tn059@utez.edu.mx>
2024-09-20 17:22:38 -0600

    [ADD] screen sales

--- src/screens/sales/SalesScreen.jsx
+++ src/screens/sales/SalesScreen.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import {Text} from "react-native";
+import {SafeAreaView, StyleSheet, Text} from "react-native";
 
 const SalesScreen = () => {
     return (
-        <>
+        <SafeAreaView style={styles.container}>
             <Text>Sales Screen</Text>
-        </>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
 
 export default SalesScreen;
